dbc4b947d338e9b57c5879f13609e2a601cec5e1
ᴍɪɢᴜᴇʟ ᴏ’ʜᴀʀᴀ! <anmol0700k@gmail.com>
2025-05-11 16:18:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/Jisshubot/Jisshu-forward-bot Jisshubot/Jisshu-forward-bot 
+  git clone https://github.com/Miguel-Hara/Premium-forward-bot Miguel-Hara/Premium-forward-bot 
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/Jisshubot/Jisshu-forward-bot -b $BRANCH /Jisshu-forward-bot
+  git clone https://github.com/Miguel-Hara/Premium-forward-bot -b $BRANCH /Premium-forward-bot
 fi
-cd Jisshubot/Jisshu-forward-bot 
+cd Miguel-Hara/Premium-forward-bot 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 gunicorn app:app & python3 main.py
